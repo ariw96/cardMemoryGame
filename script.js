e2109729a571151640b45127f8e0f038cfe22666
ahmad-mhajna <ahmad.mhmad.rashed777@gmail.com>
2022-05-01 17:03:49 +0300

    difficulty functionality + comments on functions

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const cards = document.querySelectorAll(".memory-card");
 const newGame = document.querySelector(".newGame");
+const difficultsArray = [...document.querySelectorAll(".difficult")];
+let cards = document.querySelectorAll(".memory-card");
 
+let selected = "";
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
@@ -8,6 +10,40 @@ let correct = 0;
 let wrong = 0;
 let score = 0;
 const highScore = window.localStorage.getItem("highScore") || 0;
+
+// Difficulty button event listener, On Activation it sets the grid size
+difficultsArray.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    selected =
+      event.target.classList[1] === "easy"
+        ? 6
+        : event.target.classList[1] === "medium"
+        ? 9
+        : 12;
+    generateBoard();
+  });
+});
+
+// Board Generation,this takes grid size and generates a board
+function generateBoard() {
+  document.querySelector(".memory-game").innerText = "";
+  for (let i = 1; i <= selected; i++) {
+    for (let j = 0; j < 2; j++) {
+      const card = document.createElement("div");
+      card.classList.add("memory-card");
+      const frontFace = document.createElement("div");
+      frontFace.className = `front-face image${i}`;
+      const backFace = document.createElement("div");
+      backFace.className = `back-face`;
+      card.append(frontFace, backFace);
+      card.addEventListener("click", flipCard);
+      document.querySelector(".memory-game").append(card);
+    }
+  }
+  cards = document.querySelectorAll(".memory-card");
+  shuffle();
+}
+// this flips the card
 function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
@@ -24,6 +60,7 @@ function flipCard() {
   secondCard = this;
   checkForMatch();
 }
+// this function updates all text in the website
 function updatetext() {
   const correctText = document.querySelector(".correct");
   const wrongText = document.querySelector(".wrong");
@@ -34,18 +71,20 @@ function updatetext() {
   scoreText.innerText = `Score:${score}`;
   highScoreText.innerText = `High Score:${highScore}`;
 }
-
+// this function sets the localStorage high score
 function setHighScore() {
   if (score > highScore) window.localStorage.setItem("highScore", score);
 }
+// this function checks for a match between the 2 selected cards if true adds to correct guesses if not adds to wrong guesses
 function checkForMatch() {
-  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  let isMatch =
+    firstCard.children[0].classList[1] === secondCard.children[0].classList[1];
 
-  isMatch ? disableCards() : unflipCards();
+  isMatch ? onCorrect() : onWrong();
   updatetext();
 }
-
-function disableCards() {
+//this function runs when a match happens
+function onCorrect() {
   firstCard.removeEventListener("click", flipCard);
   secondCard.removeEventListener("click", flipCard);
   correct++;
@@ -56,8 +95,8 @@ function disableCards() {
     setHighScore();
   }
 }
-
-function unflipCards() {
+// this function runs in case there isn't a match
+function onWrong() {
   lockBoard = true;
   wrong++;
   score -= 10;
@@ -68,17 +107,15 @@ function unflipCards() {
     resetSelection();
   }, 1500);
 }
-
+// this function resets the selection of the 2 cards
 function resetSelection() {
   [hasFlippedCard, lockBoard] = [false, false];
   [firstCard, secondCard] = [null, null];
 }
-
-(function shuffle() {
+// this function shuffles the board
+function shuffle() {
   cards.forEach((card) => {
     let randomPos = Math.floor(Math.random() * 12);
     card.style.order = randomPos;
   });
-})();
-
-cards.forEach((card) => card.addEventListener("click", flipCard));
+}
